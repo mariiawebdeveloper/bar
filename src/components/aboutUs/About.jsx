73c3cfec9bd7039da79ff6825730a0e9a6409be6
MariiaMarchenko <96719405+MariiaMarchenko@users.noreply.github.com>
2023-04-14 00:29:32 +0200

    clean outdata

--- src/components/aboutUs/About.jsx
+++ src/components/aboutUs/About.jsx
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import './About.css'
+
 class About extends Component {
     render() {
         return (
             <div>
-ABOUT
+                ABOUT
             </div>
         );
     }
